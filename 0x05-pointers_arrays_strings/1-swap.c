d0117340be6d19c5cd39473c5ee3d8727583d6b6
SylvesterOtuGraham <grahamsylvester694@gmail.com>
2023-02-28 13:55:51 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include"main,h"
+#include<stdio.h>
+/**
+ * swap_int- uses the pointers to swap values in diferent addresses
+ * @int *a - its a pointer
+ * @int *b -its a pointer
+ * return: 0 if succesful
+ */
+void swap_int(int *a, int *b);
+{
+	int c = int d;
+	int d = int c;
+
+	return (0);
+}
