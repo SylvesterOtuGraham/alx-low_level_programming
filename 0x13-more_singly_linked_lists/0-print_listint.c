3a39a3bed9f76c6cd2a68cd501d1515e5fc1b08f
SylvesterOtuGraham <grahamsylvester694@gmail.com>
2023-04-04 22:30:31 -0700

    element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t num_1 = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		num_1++;
 	}
 
-	return (count);
+	return (num_1);
 }
