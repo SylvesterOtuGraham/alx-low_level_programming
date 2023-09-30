1dcb386ed31127dc0232a4de25b0dcdafa085d99
SylvesterOtuGraham <grahamsylvester694@gmail.com>
2023-09-30 07:02:56 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,17 +4,6 @@
  * unsigned int binary_to_uint(const char *b) - converts binary to integer
  * int main (void)- calls the function
  */
-unsigned int binary_to_uint(const char *b);
-int main(void)
-{
-const char *binary = "1011";
-
-unsigned int n;
-
-n = binary_to_uint(binary);
-printf("%u\n", n);
-}
-
 unsigned int binary_to_uint(const char *b)
 {int weight = 1;
 
