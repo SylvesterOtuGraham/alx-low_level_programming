b9e3ddc9dd2c354cec729c3584f495e78dddf8b1
SylvesterOtuGraham <grahamsylvester694@gmail.com>
2023-10-02 12:49:02 -0700

    binarytoint

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int binary;
+
+	for (i = 63; i >= 0; i--)
+	{
+		binary  = n >> i;
+
+		if (binary & 1)
+		{
+			printf("1");
+		}
+		else
+			printf("0");
+	}
+
+}
